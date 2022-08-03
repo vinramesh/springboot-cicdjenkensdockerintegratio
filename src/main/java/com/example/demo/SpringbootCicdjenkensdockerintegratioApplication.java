7430a3c5cb70756807914df1640fe126454a9163
vinramesh <vin.ramesh@gmail.com>
2022-08-03 15:39:43 +0800

    saddadas

--- src/main/java/com/example/demo/SpringbootCicdjenkensdockerintegratioApplication.java
+++ src/main/java/com/example/demo/SpringbootCicdjenkensdockerintegratioApplication.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootCicdjenkensdockerintegratioApplication {
 
+	@GetMapping("/welcome")
+	public String getWelcomeMessage() {
+		return "Welcome to micro servers ";
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(" ============== welcome ======================");
 
