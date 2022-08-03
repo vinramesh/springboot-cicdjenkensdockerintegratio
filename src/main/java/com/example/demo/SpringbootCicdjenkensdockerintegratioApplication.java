fadfafd0c0e0c2f03b6c4e8793f1c478ac66e682
vinramesh <vin.ramesh@gmail.com>
2022-08-03 09:18:50 +0800

    controller class got changed

--- src/main/java/com/example/demo/SpringbootCicdjenkensdockerintegratioApplication.java
+++ src/main/java/com/example/demo/SpringbootCicdjenkensdockerintegratioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCicdjenkensdockerintegratioApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" ============== welcome ======================");
 		SpringApplication.run(SpringbootCicdjenkensdockerintegratioApplication.class, args);
 	}
 
