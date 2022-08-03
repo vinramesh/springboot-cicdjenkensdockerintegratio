e5b108d1d41d6d299bfd44df5a39c957ea1799a7
vinramesh <vin.ramesh@gmail.com>
2022-08-03 16:12:28 +0800

    55555555555555

--- src/main/java/com/example/demo/SpringbootCicdjenkensdockerintegratioApplication.java
+++ src/main/java/com/example/demo/SpringbootCicdjenkensdockerintegratioApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/test")
 public class SpringbootCicdjenkensdockerintegratioApplication {
 
 	@GetMapping("/welcome")
